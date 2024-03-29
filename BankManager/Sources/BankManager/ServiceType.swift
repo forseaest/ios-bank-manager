ee0f0ba69e933947599bf404cb3a7c1fb865f4aa
yawoong2 <adamzoong@naver.com>
2024-03-30 00:12:55 +0900

    add: 은행 업무를 구분하기 위한 ServiceType enum 타입 생성

---
+++ BankManager/Sources/BankManager/ServiceType.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by JIWOONG on 2024/03/29.
+//
+
+import Foundation
+
+enum ServiceType {
+    case deposit
+    case loan
+    
+    var name: String {
+        switch self {
+        case .deposit :
+            return "예금"
+        case .loan :
+            return "대출"
+        }
+    }
+}
